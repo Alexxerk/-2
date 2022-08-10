9e7568d1d0f73bc1bf344f94585566ec71e9c511
Alex <newalexis777@gmail.com>
2022-08-10 23:39:40 +0400

    task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите день недели: ");
+int x = int.Parse(Console.ReadLine());
+
+if (x == 1)
+{
+    Console.WriteLine("Monday");
+}
+else if (x == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+else if (x == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+else if (x == 4)
+{
+    Console.WriteLine("Thursday");
+}
+else if (x == 5)
+{
+    Console.WriteLine("Friday");
+}
+else if (x == 6)
+{
+    Console.WriteLine("Saturday - weekend!");
+}
+else if (x == 7)
+{
+    Console.WriteLine("Sunday - weekend!");
+}
+else 
+{
+    Console.WriteLine("Вы ввели некоректное число. ");
+}
