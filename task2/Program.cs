2df0525e9434a4df9e4cb4ffd09bee6877bc92cd
Alex <newalexis777@gmail.com>
2022-08-11 00:16:36 +0400

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Выясняем является ли 1 число квадратом 2 ");
+Console.WriteLine("Введите первое число: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x == y * y)
+{
+    Console.Write("Число ");
+    Console.Write(x);
+    Console.Write(" является квадратом числа ");
+    Console.Write(y);
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(x);
+    Console.Write(" не является квадратом числа ");
+    Console.Write(y);
+}
+
+
