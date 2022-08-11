9d302431c41aa90f23a3c485630e6e0c18121e74
Alex <newalexis777@gmail.com>
2022-08-11 19:36:53 +0400

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число x: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число y: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Точка находиться в первой четверти системы координат ");
+}
+else if (x < 0 && y > 0)
+{
+   Console.WriteLine("Точка находиться во второй четверти системы координат "); 
+}
+else if (x < 0 && y < 0)
+{
+   Console.WriteLine("Точка находиться в третей четверти системы координат "); 
+}
+else if (x > 0 && y < 0)
+{
+   Console.WriteLine("Точка находиться в четвертой четверти системы координат "); 
+}
+else 
+{
+   Console.WriteLine("Точка лежит на оси координат ");
+}
