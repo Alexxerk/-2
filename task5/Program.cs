394e81fc84ce1cb619fe43a13270bf0a6e77e557
Alex <newalexis777@gmail.com>
2022-08-11 20:04:50 +0400

    task5

---
+++ task5/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите количество последовательных чисел от 1 которые нужно возвести в куб ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+int x = 1;
+int y = 0;
+
+while(x <= n - 1)
+{
+y = x * x * x;
+x = x + 1;
+Console.Write(y);
+Console.Write(", ");
+}
+
+y = n * n * n;
+Console.Write(y);
+
