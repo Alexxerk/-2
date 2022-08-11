7d9ef2989c01a7ecdc156987b7b29ff5778bc239
Alex <newalexis777@gmail.com>
2022-08-11 21:02:25 +0400

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите количество последовательных чисел от 1: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите шаг последовательности: ");
+int z = int.Parse(Console.ReadLine());
+int[] array = new int[a];
+int x = 1;
+int count = 1;
+int sum = 0;
+
+while(count < a)
+{
+    x = x + z;
+    count++;
+}
+
+sum = ((1 + x) * a) / 2;
+
+
+
+Console.Write("Сумма чисел данной арифметической последовательности ==  ");
+Console.Write(sum);
