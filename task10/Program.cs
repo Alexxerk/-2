9132d7405a1e76c12d469967f471a82d0ef5c6cb
Alex <newalexis777@gmail.com>
2022-08-13 23:20:27 +0400

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,27 @@
+﻿int Input()
+{
+    Console.WriteLine("Введите последовательность от 1 до n: ");
+    int n = int.Parse(Console.ReadLine());
+    return n;
+}
+void Decision(int n)
+{
+    int y = 1;
+    int count = 1;
+    while (count <= n)
+    {
+        int x = y * y * y;
+        y++;
+        count++;
+        if(x % 2 == 0)
+        {
+            Console.Write(x);
+            Console.Write(" ");
+        }
+        
+    }
+}
+
+int input = Input();
+Console.Write("Кубы чисел заканчивающихся на четную цифру: ");
+Decision(input);
