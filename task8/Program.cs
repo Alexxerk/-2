2a4539d73aaa9f3d00891399b2a203e7642f153f
Alex <newalexis777@gmail.com>
2022-08-13 20:32:25 +0400

    task8

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,6 +1,12 @@
-﻿void Decision()
+﻿int Input()
 {
-    int x = 123;
+Console.WriteLine("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+return x;
+}
+
+int Size(int x)
+{
     int whole = x;
     int count = 0;
     
@@ -9,12 +15,32 @@
         whole = whole / 10;
         count++;
     }
-    
-    Console.Write("В числе ");
-    Console.Write(x);
-    Console.Write(" количество цифр входящих в него ");
-    Console.Write(count);
-
+    return count;
 }
 
-Decision();
+void Sum(int count, int x)
+{
+    int y = x % 10;
+    int z = x / 10;
+    int a = y;
+    int count2 = 1;
+    while (count2 < count)
+    {
+        y = z % 10;
+        z = z / 10;
+        a = a + y;
+        count2++;
+    }
+    Console.Write("Сумма цифр входящих в число == ");
+    Console.Write(a);
+}
+
+int input = Input();
+int size = Size(input);
+
+Sum(size, input);
+
+
+
+
+
