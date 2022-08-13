a9d76422a778c41956498a2c5d9ff627d8de0577
Alex <newalexis777@gmail.com>
2022-08-13 15:33:06 +0400

    task7

---
+++ task7/Program.cs
@@ -0,0 +1,27 @@
+﻿int Number()
+{
+    Console.WriteLine("Введите число: ");
+    int x = int.Parse(Console.ReadLine());
+    return x;
+}
+
+void Decision(int x)
+{
+    int whole = x;
+    int count = 0;
+    
+    while (whole != 0)
+    {
+        whole = whole / 10;
+        count++;
+    }
+    
+    Console.Write("В числе ");
+    Console.Write(x);
+    Console.Write(" количество цифр входящих в него ");
+    Console.Write(count);
+
+}
+
+Decision(Number());
+
---
+++ task8/Program.cs
@@ -0,0 +1,20 @@
+﻿void Decision()
+{
+    int x = 123;
+    int whole = x;
+    int count = 0;
+    
+    while (whole != 0)
+    {
+        whole = whole / 10;
+        count++;
+    }
+    
+    Console.Write("В числе ");
+    Console.Write(x);
+    Console.Write(" количество цифр входящих в него ");
+    Console.Write(count);
+
+}
+
+Decision();
