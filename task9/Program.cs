e1a23ee087c30c2a9a6885a4cd09e6e0eb81bd53
Alex <newalexis777@gmail.com>
2022-08-13 22:43:42 +0400

    task9

---
+++ task9/Program.cs
@@ -0,0 +1,25 @@
+﻿int Input()
+{
+    Console.WriteLine("Введите количество чисел от 1: ");
+    int n = int.Parse(Console.ReadLine());
+    return n;
+}
+void Decision(int n)
+{
+    int count = 1;
+    int x = 1;
+    int z = 2;
+    while (count < n)
+    {
+        x = x * z;
+        z++;
+        count++;
+    }
+    Console.Write("Произведение последовательных чисел от 1 до ");
+    Console.Write(n);
+    Console.Write(" == ");
+    Console.Write(x);
+}
+
+int input = Input();
+Decision(input);
